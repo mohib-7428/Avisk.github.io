ff0e8bb90323e7914ffd4e87902efa21ce7c5129
Wembo XD <github.a084@gmail.com>
2026-03-11 03:28:48 +0400

    Reworked Languages-index to follow new icons from devicon.com

--- languages-index.ts
+++ languages-index.ts
@@ -1,20 +1,14 @@
-import { HTMLConfig } from './languages/HTML-CSS-JS';
-import { JSConfig } from './languages/JS';
-import { CPPConfig } from './languages/C++';
-import { JavaConfig } from './languages/Java';
-import { WASMConfig } from './languages/WASM';
-import { MojoConfig } from './languages/Mojo';
-import { RubyConfig } from './languages/Ruby';
-import { PhpConfig } from './languages/Php';
-import { ErLangConfig } from './languages/ErLang';
-import { AdaConfig } from './languages/Ada';
-import { CoffeeScriptConfig } from './languages/CoffeeScript';
-import { LuaConfig } from './languages/Lua';
-import { AppleScriptConfig } from './languages/AppleScript';
-
 export const allLanguages = [
-  HTMLConfig, JSConfig, CPPConfig, JavaConfig, 
-  WASMConfig, MojoConfig, RubyConfig, PhpConfig, 
-  ErLangConfig, AdaConfig, CoffeeScriptConfig, 
-  LuaConfig, AppleScriptConfig
+  { name: "HTML/CSS/JS", iconClass: "devicon-html5-plain", boilerplate: "" },
+  { name: "TypeScript", iconClass: "devicon-typescript-plain", boilerplate: "// TS Scripting in AVISK" },
+  { name: "C++", iconClass: "devicon-cplusplus-plain", boilerplate: "#include <iostream>\nusing namespace std;" },
+  { name: "Java", iconClass: "devicon-java-plain", boilerplate: "public class Main {\n  public static void main(String[] args) {}\n}" },
+  { name: "WebAssembly", iconClass: "devicon-wasm-original", boilerplate: "(module)" },
+  { name: "Ruby", iconClass: "devicon-ruby-plain", boilerplate: "puts 'AVISK IDE'" },
+  { name: "PHP", iconClass: "devicon-php-plain", boilerplate: "<?php echo 'AVISK'; ?>" },
+  { name: "Lua", iconClass: "devicon-lua-plain", boilerplate: "-- Lua Script" },
+  { name: "Erlang", iconClass: "devicon-erlang-plain", boilerplate: "% Erlang Module" },
+  { name: "CoffeeScript", iconClass: "devicon-coffeescript-original", boilerplate: "# CoffeeScript" },
+  { name: "Mojo", iconClass: "devicon-code-plain", boilerplate: "fn main():" }, // Mojo icon isn't in Devicon yet, using generic
+  { name: "Ada", iconClass: "devicon-code-plain", boilerplate: "with Ada.Text_IO;" }
 ];
