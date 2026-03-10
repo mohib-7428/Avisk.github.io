4376f1f4efbb209ac5a6ae7a5f087494d296c746
Wembo XD <github.a084@gmail.com>
2026-03-11 03:23:29 +0400

    Reworking files

--- languages/Lua.tsx
+++
@@ -1,5 +0,0 @@
-export const LuaConfig = {
-  name: "Lua",
-  extension: ".lua",
-  icon: "PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAxMjggMTI4Ij48Y2lyY2xlIGN4PSI2NCIgY3k9IjY0IiByPSI2MCIgZmlsbD0iIzAwMDA4MCIvPjxjaXJjbGUgY3g9IjgwIiBjeT0iNDgiIHI9IjEwIiBmaWxsPSIjRkZGRkZGIi8+PC9zdmc+"
-};
