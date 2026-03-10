5bd341f1bac25910cb152714b64a7f2149744bb9
Wembo XD <github.a084@gmail.com>
2026-03-11 03:13:22 +0400

    The Index for the Code Languages in the IDE
    Code languages in IDE's Index

---
+++ languages-index.ts
@@ -0,0 +1,20 @@
+import { HTMLConfig } from './languages/HTML-CSS-JS';
+import { JSConfig } from './languages/JS';
+import { CPPConfig } from './languages/C++';
+import { JavaConfig } from './languages/Java';
+import { WASMConfig } from './languages/WASM';
+import { MojoConfig } from './languages/Mojo';
+import { RubyConfig } from './languages/Ruby';
+import { PhpConfig } from './languages/Php';
+import { ErLangConfig } from './languages/ErLang';
+import { AdaConfig } from './languages/Ada';
+import { CoffeeScriptConfig } from './languages/CoffeeScript';
+import { LuaConfig } from './languages/Lua';
+import { AppleScriptConfig } from './languages/AppleScript';
+
+export const allLanguages = [
+  HTMLConfig, JSConfig, CPPConfig, JavaConfig, 
+  WASMConfig, MojoConfig, RubyConfig, PhpConfig, 
+  ErLangConfig, AdaConfig, CoffeeScriptConfig, 
+  LuaConfig, AppleScriptConfig
+];
